cbc6c99cf9239dbe62541ea312004a21c92de8a9
itzVort3x1 <saikaustubh10@gmail.com>
2024-01-01 21:30:54 +0530

    finished project

---
+++ student_data/models/schoolModel.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+const schoolSchema = new mongoose.Schema({
+	schoolName: String,
+	schoolId: Number,
+	webhookDetails: [
+		{
+			eventName: String,
+			endpointUrl: String,
+		},
+	],
+});
+
+const schoolModel = mongoose.model("schools", schoolSchema);
+
+exports.schoolModel = schoolModel;
